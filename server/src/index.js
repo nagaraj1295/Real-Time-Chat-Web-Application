624fbf0660cb379bfc547ffd4534132ca0506d53
NAGARAJ C <rjnaga123@gmail.com>
2026-03-19 15:46:18 +0530

    Step 9: Production Build Support (Static File Serving)

--- server/src/index.js
+++ server/src/index.js
@@ -8,8 +8,10 @@ import { app, server } from "./lib/socket.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5001;
 
@@ -22,6 +24,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../dist", "index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
