db97b088bce80ada647003e6ec49f5f0b3764267
NAGARAJ C <rjnaga123@gmail.com>
2026-03-19 14:40:49 +0530

    Step 2: Backend Authentication System (Models, Controllers, Routes, JWT)

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,8 @@ import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
@@ -16,6 +18,8 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
 }));
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
